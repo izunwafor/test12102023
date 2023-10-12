db5a47c4f3bfbcdcbce64afaae81eea88e46768f
izunwafor <86169398+izunwafor@users.noreply.github.com>
2023-10-12 16:46:42 +0100

    Add files via upload

---
+++ src/test/java/com/izu/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.izu.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
